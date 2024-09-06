d508cfa6be60d62116fd7beb25ad2544e4b9ca7a
Alice <alice.al.moradiya@gmail.com>
2024-09-06 12:22:34 -0600

    Final Navbar

--- Frontend/src/components/navbar.jsx
+++ Frontend/src/components/navbar.jsx
@@ -1,9 +1,25 @@
 import React from "react";
 
 function navbar() {
+  const navItems = (
+    <>
+      <li>
+        <a>Home</a>
+      </li>
+      <li>
+        <a>Course</a>
+      </li>
+      <li>
+        <a>Contact</a>
+      </li>
+      <li>
+        <a>About</a>
+      </li>
+    </>
+  );
   return (
     <>
-      <div>
+      <div className="max-w-screen-2xl  container mx-auto md:px-20 px-4 ">
         <div className="navbar bg-base-100">
           <div className="navbar-start">
             <div className="dropdown">
@@ -31,52 +47,35 @@ function navbar() {
                 tabIndex={0}
                 className="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow"
               >
-                <li>
-                  <a>Item 1</a>
-                </li>
-                <li>
-                  <a>Parent</a>
-                  <ul className="p-2">
-                    <li>
-                      <a>Submenu 1</a>
-                    </li>
-                    <li>
-                      <a>Submenu 2</a>
-                    </li>
-                  </ul>
-                </li>
-                <li>
-                  <a>Item 3</a>
-                </li>
+                {navItems}
               </ul>
             </div>
-            <a className="btn btn-ghost text-xl">daisyUI</a>
+            <a className=" text-2xl font-bold cursor-pointer">Bookstore</a>
           </div>
-          <div className="navbar-center hidden lg:flex">
-            <ul className="menu menu-horizontal px-1">
-              <li>
-                <a>Item 1</a>
-              </li>
-              <li>
-                <details>
-                  <summary>Parent</summary>
-                  <ul className="p-2">
-                    <li>
-                      <a>Submenu 1</a>
-                    </li>
-                    <li>
-                      <a>Submenu 2</a>
-                    </li>
-                  </ul>
-                </details>
-              </li>
-              <li>
-                <a>Item 3</a>
-              </li>
-            </ul>
-          </div>
-          <div className="navbar-end">
-            <a className="btn">Button</a>
+          <div className="navbar-end space-x-3">
+            <div className="navbar-center hidden lg:flex">
+              <ul className="menu menu-horizontal px-1">{navItems}</ul>
+            </div>
+            <div className="hidden md:block"> {/* visible for medium screen or more. hidden from small screens */}
+              <label className="input input-bordered flex items-center gap-2">
+                <input type="text" className="grow" placeholder="Search" />
+                <svg 
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 16 16"
+                  fill="currentColor"
+                  className="h-4 w-4 opacity-70"
+                >
+                  <path
+                    fillRule="evenodd"
+                    d="M9.965 11.026a5 5 0 1 1 1.06-1.06l2.755 2.754a.75.75 0 1 1-1.06 1.06l-2.755-2.754ZM10.5 7a3.5 3.5 0 1 1-7 0 3.5 3.5 0 0 1 7 0Z"
+                    clipRule="evenodd"
+                  />
+                </svg>
+              </label>
+            </div>
+            <div>
+              <a className="bg-black text-white px-3 py-2 rounded-md hover:bg-slate-800 duration-300 cursor-pointer">Login</a>
+            </div>
           </div>
         </div>
       </div>
