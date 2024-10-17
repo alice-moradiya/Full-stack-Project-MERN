017416e4f95625b97a84940ab0917de798415305
Alice <alice.al.moradiya@gmail.com>
2024-10-17 16:25:22 -0600

    Final Home Page

--- Frontend/src/components/Cards.jsx
+++ Frontend/src/components/Cards.jsx
@@ -1,26 +1,25 @@
 import React from 'react';
 
 function Cards({item}) {
-  console.log(item)
   return (
     <>
-      <div>
-        <div className="card bg-base-100 w-96 shadow-xl">
+      <div className='mt-4 my-3 p-3'>
+        <div className="card bg-base-100 w-92 shadow-xl p-10">
           <figure>
             <img
-              src="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.webp"
+              src={item.image}
               alt="Shoes"
             />
           </figure>
           <div className="card-body">
             <h2 className="card-title">
-              Shoes!
-              <div className="badge badge-secondary">NEW</div>
+            {item.name}
+              <div className="badge badge-secondary">{item.category}</div>
             </h2>
-            <p>If a dog chews shoes whose shoes does he choose?</p>
-            <div className="card-actions justify-end">
-              <div className="badge badge-outline">Fashion</div>
-              <div className="badge badge-outline">Products</div>
+            <p>{item.tittle}</p>
+            <div className="card-actions justify-between">
+              <div className="badge badge-outline">${item.price}</div>
+              <div className="cursor-pointer px-2 py-1 rounded-full border-[2px] hover:bg-pink-500  hover:text-white duration-200">Buy now</div>
             </div>
           </div>
         </div>
--- Frontend/src/components/navbar.jsx
+++ Frontend/src/components/navbar.jsx
@@ -35,7 +35,7 @@ function navbar() {
   return (
     <>
       <div
-        className={`max-w-screen-2xl  container mx-auto md:px-20 px-4 fixed top-0 left-0 right-0 ${
+        className={`max-w-screen-2xl  container mx-auto md:px-20 px-4 fixed top-0 left-0 right-0  z-50 ${
           sticky
             ? "sticky-navbar shadow-md bg-base-300 duration-300 transition-all ease-in-out"
             : ""
